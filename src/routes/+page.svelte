e4cbf1ccc426d8760e48c9133a40a0198a51b155
Robert Nickel <github@robertnickel.online>
2022-09-24 18:02:55 +0200

    Removed static questions and dynamified the amount of total players.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,17 +3,9 @@
 
     type Answer = { text: string; player: number };
 
-    const questions: Question[] = [
-        {
-            text: "Was ist ein Kafir?",
-            answer: "Ein islamisches Wort für einen Ungläubigen.",
-        },
-        { text: "Fisole ist ein oder ist eine...", answer: "Stadt in Italien" },
-    ];
+    let totalPlayers = 0;
 
-    const totalPlayers = 5;
-
-    let currentQuestion: Question;
+    let currentQuestion: Question | null;
     let answers: Answer[];
     let activePlayer: number;
     let showAnswers: boolean;
@@ -22,19 +14,11 @@
     initRound();
 
     function initRound() {
+        currentQuestion = null;
         answers = [];
         activePlayer = 0;
         showAnswers = false;
         showCorrectAnswer = false;
-        currentQuestion = drawQuestion();
-    }
-
-    function drawQuestion() {
-        let index = Math.floor(Math.random() * questions.length);
-        let question = questions[index];
-        questions.splice(index, 1);
-        answers.push({ text: question.answer, player: 99 }); // this is the correct answer
-        return question;
     }
 
     function shuffleAnswers() {
@@ -54,50 +38,86 @@
     }
 </script>
 
-<h1>Unperfekt</h1>
+{#if totalPlayers == 0}
+    <h1>Unperfekt</h1>
 
-<h3>{currentQuestion?.text}</h3>
-
-{#if !showAnswers}
-    <input
-        id="answer_input"
-        autocomplete="off"
-        placeholder={"Du bist dran, Spieler " + (activePlayer + 1)}
-    />
+    <label for="total_players_input">Wie viele Spieler?</label>
+    <input id="total_players_input" type="number" value="4" />
     <button
         on:click={() => {
-            let answerInput = document.getElementById("answer_input");
-
-            answers.push({ text: answerInput.value, player: 1 });
-            answers = answers;
-
-            answerInput.value = "";
-            answerInput.focus();
-
-            if (activePlayer == totalPlayers - 1) {
-                shuffleAnswers();
-                showAnswers = true;
-            } else {
-                activePlayer++;
-            }
-        }}>Fertig</button
+            totalPlayers = +document.getElementById("total_players_input")
+                .value;
+        }}>Spielen</button
     >
-{/if}
+{:else if !currentQuestion}
+    <h3>Spielleiter!</h3>
+    <label for="create_question_input">Stelle eine Frage</label>
+    <input id="create_question_input" />
+    <label for="create_question_answer_input"
+        >Gib die richtige Antwort ein</label
+    >
+    <input id="create_question_answer_input" />
+    <button
+        on:click={() => {
+            let createQuestionInput = document.getElementById(
+                "create_question_input"
+            );
+            let createQuestionAnswerInput = document.getElementById(
+                "create_question_answer_input"
+            );
+            currentQuestion = {
+                text: createQuestionInput.value,
+                answer: createQuestionAnswerInput.value,
+            };
+            answers.push({ text: currentQuestion.answer, player: 99 }); // this is the correct answer
+            createQuestionInput.value = "";
+            createQuestionAnswerInput = "";
+        }}>Frage erstellen</button
+    >
+{:else}
+    <h3>{currentQuestion?.text}</h3>
 
-<div>
-    {#if showAnswers}
-        <br />
-        {#each answers as answer, i}
-            <p>
-                {i + 1 + " : " + answer.text}
-                {#if answer.player == 99 && showCorrectAnswer}⬅️{/if}
-            </p>
-        {/each}
-        {#if !showCorrectAnswer}
-            <button on:click={() => (showCorrectAnswer = true)}
-                >Zeige richtige Antwort</button
-            >
-        {:else}
-            <button on:click={initRound}>Nächste Frage</button>{/if}
+    {#if !showAnswers}
+        <label for="answer_input">{"Spieler " + (activePlayer + 1)}</label>
+        <input
+            id="answer_input"
+            autocomplete="off"
+            placeholder={"Gib eine falsche aber legitime Antwort!"}
+        />
+        <button
+            on:click={() => {
+                let answerInput = document.getElementById("answer_input");
+
+                answers.push({ text: answerInput.value, player: 1 });
+                answers = answers;
+
+                answerInput.value = "";
+                answerInput.focus();
+
+                if (activePlayer == totalPlayers - 1) {
+                    shuffleAnswers();
+                    showAnswers = true;
+                } else {
+                    activePlayer++;
+                }
+            }}>Fertig</button
+        >
     {/if}
-</div>
+
+    <div>
+        {#if showAnswers}
+            {#each answers as answer, i}
+                <p>
+                    {i + 1 + " : " + answer.text}
+                    {#if answer.player == 99 && showCorrectAnswer}⬅️{/if}
+                </p>
+            {/each}
+            {#if !showCorrectAnswer}
+                <button on:click={() => (showCorrectAnswer = true)}
+                    >Zeige richtige Antwort</button
+                >
+            {:else}
+                <button on:click={initRound}>Nächste Frage</button>{/if}
+        {/if}
+    </div>
+{/if}
